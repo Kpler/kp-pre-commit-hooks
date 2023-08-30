51f5d2303c23be1741b90499daa2477514da612d
Vincent Liu <vliu@kpler.com>
2023-08-30 12:23:07 +0800

    fix error on the list

--- kp_pre_commit_hooks/gitops-values-validation.py
+++ kp_pre_commit_hooks/gitops-values-validation.py
@@ -172,8 +172,6 @@ def main():
                     f"\nERROR: Validation errors for {service_path} in {instance_file}, {base_file} or values.yaml:"
                 )
                 for error in errors:
-                    print(find_full_error_path(error))
-                    #error_location = "/".join(find_full_error_path(error)) or "the root"
                     error_location = "/".join(str(x) for x in find_full_error_path(error)) or "the root"
                     print(f"  - {error.message} at {error_location}")
 
