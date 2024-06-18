9acd96beed8ca3951c7a413fa371c87bb5abf7b9
Vincent Liu <128127889+vl-kp@users.noreply.github.com>
2024-06-18 21:39:59 +0800

    Update gitops-values-validation.py

--- kp_pre_commit_hooks/gitops-values-validation.py
+++ kp_pre_commit_hooks/gitops-values-validation.py
@@ -241,7 +241,7 @@ class ServiceInstanceConfigValidator:
             "$.platform-managed-chart.serviceName": ["'sink' does not match the service folder name 'apply-edits-exportbol-jdbc'"]
         },
         "maritime-news-strapi-api": {
-            "$.extra": ["Additional properties are not allowed ('extra' was unexpected)"]
+            "$.extra": ["'extra' was unexpected"]
         },
     }
 
