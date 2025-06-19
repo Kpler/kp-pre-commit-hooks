4daa4691f9853748935e8de20742854e6fe8bb2f
Namsam <snaim@kpler.com>
2025-06-19 10:30:25 +0200

    chore: allow merge queue branch names

--- branch-naming.sh
+++ branch-naming.sh
@@ -3,9 +3,10 @@
 exit_status=0
 currentbranch=$(git symbolic-ref --short HEAD)
 dependabot="dependabot"
+merge-queue="gh-readonly-queue"
 # NOTE: Branches created by Dependabot does not have any limit, according to
 # https://github.com/dependabot/dependabot-core/issues/396, that's why we allow them
-if [ "$(expr length "$currentbranch")" -gt 70 ] && [[ ! "$currentbranch" =~ $dependabot ]]
+if [ "$(expr length "$currentbranch")" -gt 70 ] && [[ ! "$currentbranch" =~ $dependabot ]] && [[ ! "$currentbranch" =~ merge-queue ]]
 then
   exit_status=1
   echo "Branch name too long, should be less than 70 characters."
